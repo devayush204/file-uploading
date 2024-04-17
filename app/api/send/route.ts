d079ce37901592cf839fe8955e9fc39cd8647f6a
ayush <ayushbhatt@204gmail.com>
2024-04-17 10:12:34 +0530

    updated

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -3,17 +3,22 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const data = await resend.emails.send({
-      from: 'veronica@resend.dev',
-      to: ['veronfoodie402@gmail.com'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+export async function POST(req) {
+    const response = await req.json()
+    try {
+        const { data, error } = await resend.emails.send({
+            from: 'veronica@resend.dev',
+            to: response.emailToSend,
+            subject: response?.userName + " send you a file",
+            react: EmailTemplate({ response }),
+        });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error });
-  }
+        if (error) {
+            return Response.json({ error });
+        }
+
+        return Response.json({ data });
+    } catch (error) {
+        return Response.json({ error });
+    }
 }
