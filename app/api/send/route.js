414e0ffa2932ac31a5cb748e7a85b534e06ba882
ayush <ayushbhatt@204gmail.com>
2024-04-17 10:32:02 +0530

    4

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,7 +1,7 @@
 import { EmailTemplate } from '../../_components/Email-template';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_faSVjkbi_MFQ8Vpcq6JHHxZ5Ke2avzmWq");
 
 export async function POST(req) {
     try {
