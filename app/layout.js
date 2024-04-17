3235f9de4de99b604b8be5d5fc272bf7e02ef3bd
ayush <ayushbhatt@204gmail.com>
2024-04-17 11:08:05 +0530

    7

--- app/layout.js
+++ app/layout.js
@@ -11,10 +11,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    // <ClerkProvider > 
+    <ClerkProvider > 
       <html lang="en">
         <body className={inter.className}>{children}</body>
       </html>
-    // </ClerkProvider>
+    </ClerkProvider>
   );
 }
